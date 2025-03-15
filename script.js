1ea541e01d69468d5f9090a5665386b537468962
Eshan Bhuse <eshan.bhuse@spit.ac.in>
2025-03-15 22:24:00 +0530

    Made changes in font and color also added contact form and some css style

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function toggleMenu()
     icon.classList.toggle("open")
 
 }
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzvdVaS5QjLy7_qzGceoPRLuEIJjbM_Tp6A0inZC5t1BSyVfidSQYQxIbK-y5CCCWPpyA/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  const message = document.getElementById("message")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        message.innerHTML = "You have sent the message successfully"
+        setTimeout(function(){
+          message.innerHTML = ""
+        }, 1000);
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
